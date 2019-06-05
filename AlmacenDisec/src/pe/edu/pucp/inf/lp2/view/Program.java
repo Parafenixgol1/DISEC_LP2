40ea57e78f54ce5201f0a7d125ef0ebe965fec00
VictorAlonsoVergara <a20141826@pucp.pe>
2019-06-04 21:54:10 -0500

    Primer reporte OITXCliente2

--- AlmacenDisec/src/pe/edu/pucp/inf/lp2/view/Program.java
+++ AlmacenDisec/src/pe/edu/pucp/inf/lp2/view/Program.java
@@ -5,6 +5,16 @@
  */
 package pe.edu.pucp.inf.lp2.view;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.HashMap;
+import javax.swing.JOptionPane;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
 /**
  *
  * @author Victor Alonso Vergara 20141826
@@ -15,7 +25,27 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con= DriverManager.getConnection("jdbc:mysql://sallka.lab.inf.pucp.edu.pe:3306/inf282g5","inf282g5", "Cs0omP");
+            
+        JasperReport reporte = (JasperReport) JRLoader.loadObjectFromFile(Program.class.getResource("/pe/edu/pucp/inf/lp2/reports/OITCustomerReport.jasper").getFile());
+        
+        HashMap hm = new HashMap();
+        String codigo =  JOptionPane.showInputDialog(null,"Ingrese un código de cliente:");
+        hm.put("CODIGOCLIENTE",codigo);
+        
+        JasperPrint impresion = JasperFillManager.fillReport(reporte, hm, con);
+        
+        JasperViewer visor = new JasperViewer(impresion);
+        
+        visor.setVisible(true);
+      
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    
     }
     
 }
